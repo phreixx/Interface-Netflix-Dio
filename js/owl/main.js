e12155e21e24b893fca5f1f0dc2026c82c1f8925
phreixx <phmendez@gmail.com>
2021-09-20 22:40:12 -0300

    Adicionando + carroséis

--- js/owl/main.js
+++ js/owl/main.js
@@ -2,6 +2,10 @@ $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
     nav:false,
+    video:true,
+    lazyLoad:true,
+    center:true,
+    
     responsive:{
         0:{
             items:1
